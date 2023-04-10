333aff4b2c9692de83d614af4fd4a6f79c75fce6
Jehider Pinto <jmpm0409@gmail.com>
2023-04-10 17:18:56 -0500

    Ejercicio 1

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 // Este es el mensaje de bienvenida
+/*
 console.log("Esta es la puerta de entrada del proyecto")
 
-/*
+
 Esta es la parte intermedia de nuestro proyecto
 
 Zona de chequeos
 
 Por ultimo vamos a la ultima linea del saludo
+
+
+Este va a ser el mensaje que se envía para despedir
+console.log("Adiós")
 */
 
-// Este va a ser el mensaje que se envía para despedir
-console.log("Adiós")
+//Ejercicio1
+console.log("Hola, este es mi primer ejercicio con Node en el mejor Bootcamp de programación del mundo")

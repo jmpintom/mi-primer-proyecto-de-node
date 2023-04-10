5f3f994e41ca4aed80811186cac3b90d4f82b151
Jehider Pinto <jmpm0409@gmail.com>
2023-04-10 16:44:01 -0500

    comentarios

---
+++ index.js
@@ -0,0 +1,13 @@
+// Este es el mensaje de bienvenida
+console.log("Esta es la puerta de entrada del proyecto")
+
+/*
+Esta es la parte intermedia de nuestro proyecto
+
+Zona de chequeos
+
+Por ultimo vamos a la ultima linea del saludo
+*/
+
+// Este va a ser el mensaje que se envía para despedir
+console.log("Adiós")
